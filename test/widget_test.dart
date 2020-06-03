c1521ee83f2855cefe174ed83556ab33081d7637
mtric <29257165+mtric@users.noreply.github.com>
2020-06-03 21:13:01 +0200

    changed to new package name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:swoppyappflutter/main.dart';
+import 'package:Swoppy/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
